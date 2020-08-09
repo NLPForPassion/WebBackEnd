f59df91588258fe93a48e85a706e222982ef126d
hoangminhan <hoangminhan13g@gmail.com>
2020-08-09 23:47:29 +0700

    delete

--- controller/index.js
+++
@@ -1,6 +0,0 @@
-var express = require('express')
-var router =express.Router()
-var hoangan =require('../models/index')
-router.get('/home',hoangan.viewhome)
-
-module.exports = router
--- models/index.js
+++
@@ -1,3 +0,0 @@
-module.exports.viewhome =function(req,res){
-    res.render('pages/home')
-}
